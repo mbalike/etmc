229ea1389252b7cc63fff7c72317fd32e233db9c
mbalike <dmbalike@outlook.com>
2025-01-11 12:42:59 +0300

    roles issues resolution one

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -8,9 +8,10 @@ use App\Models\User;
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Forms\Components\TextInput;
-use Filament\Forms\Form\Components\Select;
+use Filament\Forms\Components\Select;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
@@ -46,7 +47,9 @@ class UserResource extends Resource
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('id'),
+                TextColumn::make('name'),
+                TextColumn::make('email'),
             ])
             ->filters([
                 //
