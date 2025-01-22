b6e0edae4c246c45e190a80a9cec2e3f4c0e1c74
mbalike <dmbalike@outlook.com>
2025-01-22 23:51:29 +0300

    intiaizing families

--- app/Models/Family.php
+++ app/Models/Family.php
@@ -2,9 +2,29 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Family extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'address',
+        'head_id'
+    ];
+
+    public function head(): BelongsTo
+    {
+        return $this->belongsTo(Member::class, 'head_id');
+    }
+
+    public function members(): HasMany
+    {
+        return $this->hasMany(Member::class);
+    }
 }
