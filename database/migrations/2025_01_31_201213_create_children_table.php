9de5ee4b2509e338477b6246a7d5e7545b3a76b8
mbalike <dmbalike@outlook.com>
2025-01-31 23:17:37 +0300

    children migration created

---
+++ database/migrations/2025_01_31_201213_create_children_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('children', function (Blueprint $table) {
+            $table->id();
+            $table->text('first_name');
+            $table->text('last_name');
+            $table->enum('gender',['male','female']);
+            $table->date('birthdate');
+            $table->foreignId('father_id')->nullable()->constrained('members');
+            $table->foreignId('mother_id')->nullable()->constrained('members');
+            $table->foreignId('supervisor_id')->constrained('users');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('children');
+    }
+};
